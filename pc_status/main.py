aa1f536bae645074dae55634a21f6a143db93b28
dreamer20 <gv4alex@gmail.com>
2022-06-07 19:14:54 +0300

    Add functionality to add data to app

--- pc_status/main.py
+++ pc_status/main.py
@@ -13,4 +13,65 @@ bp = Blueprint('main', __name__, url_prefix='/')
 def index():
     db = get_db()
     sys_info = db.execute('SELECT * FROM pc_status WHERE cur_date = (SELECT max(cur_date) from pc_status)').fetchone()
+    
     return render_template('index.html', sys_info=sys_info)
+
+
+@bp.route('/add')
+def add():
+    cur_date = time.time()
+    cpu_temp = request.args.get('cpu_temp')
+    ssd_temp  = request.args.get('ssd_temp')
+    cpu_fan  = request.args.get('cpu_fan')
+    add_fan  = request.args.get('add_fan')
+    ram_total  = request.args.get('ram_total')
+    ram_available  = request.args.get('ram_available')
+    disk_space_total  = request.args.get('disk_space_total')
+    disk_space_available  = request.args.get('disk_space_available')
+    uptime  = request.args.get('uptime')
+    load_average  = request.args.get('load_average')
+    last_update  = request.args.get('last_update')
+    volume_level  = request.args.get('volume_level')
+    process_count  = request.args.get('process_count')
+    keyboard_layout  = request.args.get('keyboard_layout')
+
+    sys_info = [
+        cur_date,
+        cpu_temp,
+        ssd_temp,
+        cpu_fan,
+        add_fan,
+        ram_total,
+        ram_available,
+        disk_space_total,
+        disk_space_available,
+        uptime,
+        load_average,
+        last_update,
+        volume_level,
+        process_count,
+        keyboard_layout
+    ]
+
+    db = get_db()
+    db.execute('''
+        INSERT INTO pc_status (
+            cur_date,
+            cpu_temp,
+            ssd_temp,
+            cpu_fan,
+            add_fan,
+            ram_total,
+            ram_available,
+            disk_space_total,
+            disk_space_available,
+            uptime,
+            load_average,
+            last_update,
+            volume_level,
+            process_count,
+            keyboard_layout) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', sys_info)
+    db.commit()
+
+    return 'Successful.'
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,71 @@
+import pytest
+
+
 def test_index(client):
     response = client.get('/')
 
     assert response.status_code == 200
     assert b'1654538958' in response.data
+
+
+def test_add(client):
+    query_params = {
+        'cpu_temp': 53.8,
+        'ssd_temp': 34.9,
+        'cpu_fan': 4078,
+        'add_fan': 866,
+        'ram_total': 16310072,
+        'ram_available': 13250532,
+        'disk_space_total': '29G',
+        'disk_space_available': '9,1G',
+        'uptime': '26',
+        'load_average': ' 0,06 0,23 0,29',
+        'last_update': '2022-06-06 20:42:44',
+        'volume_level': 58,
+        'process_count': 305,
+        'keyboard_layout': 'us'
+        }
+
+    response = client.get('/add', query_string=query_params)
+
+    assert response.status_code == 200
+    assert b'Successful.' in response.data
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert b'13250532' in response.data
+    assert b' 0,06 0,23 0,29' in response.data
+    assert b'2022-06-06 20:42:44' in response.data
+
+
+def test_add_empty_data(client):
+    query_params = {
+        'cpu_temp': '',
+        'ssd_temp': '',
+        'cpu_fan': '',
+        'add_fan': '',
+        'ram_total': '',
+        'ram_available': '',
+        'disk_space_total': '',
+        'disk_space_available': '',
+        'uptime': '',
+        'load_average': '',
+        'last_update': '',
+        'volume_level': '',
+        'process_count': '',
+        'keyboard_layout': ''
+        }
+
+    response = client.get('/add', query_string=query_params)
+
+    assert response.status_code == 200
+    assert b'Successful.' in response.data
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert b'-%' in response.data
+    assert b'- / -' in response.data
+    assert b'- PRM' in response.data
+    assert b'-&#8451;' in response.data
